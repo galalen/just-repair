21ba3097ab01cd79f54e7357c29d94257c41cb6c
Galalen <mohammedgalalen@gmail.com>
2020-02-01 19:43:34 +0200

    offical player in the scence

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public Rigidbody2D  body;
+	public float speed=10;
+	public float enemy_moving = -2;
+	public bool is_moving = true;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SetRandomPos",0.0f,0.2f);
+		body = GetComponent<Rigidbody2D> ();
+		//Random.Range();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void SetRandomPos() {
+		if (is_moving) {
+			Vector2 temp = new Vector3 (transform.position.x + (enemy_moving * Time.deltaTime), transform.position.y, transform.position.z);
+		 
+			
+			transform.position = temp;
+		} else {
+			is_moving = !is_moving;
+			enemy_moving *= -1;
+		}
+	}
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log ("Touch the collider");		
+		if (collider.gameObject.tag == "enemy") {
+
+
+
+			is_moving = false;
+			//Vector2 temp = new Vector2 (transform.position.x, transform.position.y + 1);
+
+			//enemy.transform.position = temp;
+
+
+			//Debug.Log (enemy.tag);
+			//enemy.is_moving = false;
+
+		}
+	}
+	void OnColliderEnter2D (Collider2D collider)
+	{
+		Debug.Log ("Touch the collider");		
+		if (collider.gameObject.tag == "ground") {
+
+
+
+			is_moving = false;
+			//Vector2 temp = new Vector2 (transform.position.x, transform.position.y + 1);
+
+			//enemy.transform.position = temp;
+
+
+			//Debug.Log (enemy.tag);
+			//enemy.is_moving = false;
+
+		}
+	}
+	void OnCollisionEnter2D (Collision2D collider)
+	{
+		Debug.Log ("Touch the collider");		
+		if (collider.gameObject.tag == "ground") {
+
+
+
+			is_moving = false;
+			//Vector2 temp = new Vector2 (transform.position.x, transform.position.y + 1);
+
+			//enemy.transform.position = temp;
+
+
+			//Debug.Log (enemy.tag);
+			//enemy.is_moving = false;
+
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private static float BLOCK_Y = -5.0F;
+
+    public GameObject block;
+    public GameObject blockHolder;
+    public int speed = 10;
+    public bool haveBlock;
+
+    private Rigidbody2D rigidbody2d;
+    private GameManager gameManager;
+    
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D> ();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        haveBlock = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameManager.gameOver) 
+        {
+            Movement();
+            DropBlock();
+        }
+    }
+
+    private void Movement() 
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        } 
+
+        if (Input.GetKeyDown(KeyCode.D)) 
+        {
+            transform.Translate(Vector3.left * -speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void DropBlock() {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (haveBlock)
+            {
+                Instantiate(block, new Vector3(transform.position.x, BLOCK_Y, 0), Quaternion.identity);
+                haveBlock = false;
+                blockHolder.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Collection") {
+            haveBlock = true;
+            blockHolder.SetActive(true);
+        }
+    }
+
+}
